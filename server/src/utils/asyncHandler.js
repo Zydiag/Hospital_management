b676c37cc3be0c93ecd927bf4f061fe97c375254
Sahil <dshiv867@gmail.com>
2024-04-19 03:56:31 +0530

    added api utils

---
+++ server/src/utils/asyncHandler.js
@@ -0,0 +1,13 @@
+const asyncHandler = (requestHandler) => {
+  return async (req, res, next) => {
+    try {
+      return await Promise.resolve(requestHandler(req, res, next));
+    } catch (err) {
+      return next(err);
+    }
+  };
+};
+
+// hello
+
+export { asyncHandler };

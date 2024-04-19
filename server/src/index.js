c9f2de820ffa112a84ffb18f184ddfc5e7557f06
Sahil <dshiv867@gmail.com>
2024-04-19 23:45:08 +0530

    revert to js

--- server/src/index.js
+++ server/src/index.js
@@ -3,5 +3,5 @@ import { app } from './app';
 const port = process.env.PORT;
 
 app.listen(port, () => {
-    return console.log(`Server started on Port: ${port}`);
+  return console.log(`Server started on Port: ${port}`);
 });

a05109a1ce121cdc25ba3fc6cf01bd9c2a912fac
PratikSanap <99598917+PratikSanap@users.noreply.github.com>
2024-05-21 00:09:34 +0530

    Update patient.route.js

--- server/src/routes/patient.route.js
+++ server/src/routes/patient.route.js
@@ -1,7 +1,12 @@
 import express from 'express';
-import { profilepatient, loginpatient } from '../controllers/patientcontroller.js'; // Updated import path
+import {
+ profilepatient,loginpatient,getpersonalinfo,getHealthRecord
+  
+} from '../controllers/patientcontroller.js'; // Updated import path
 
 const router = express.Router();
-router.post('/patientprofile', profilepatient);
-router.get('/loginpatient', loginpatient);
+router.post('/patientprofile',profilepatient);
+router.get('/loginpatient',loginpatient);
+router.get('/personal-info',getpersonalinfo);
+router.get('/healthRecord',getHealthRecord);
 export default router;

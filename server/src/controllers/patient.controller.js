15e511e38ae049c8c19a9d6842c6bb41a1482020
Sahil <dshiv867@gmail.com>
2024-05-20 21:09:47 +0530

    updated schema && changed fileNames && changed prettier 100col

---
+++ server/src/controllers/patient.controller.js
@@ -0,0 +1,99 @@
+import { ApiResponse } from '../utils/apiResponse.js';
+import { asyncHandler } from '../utils/asyncHandler.js';
+import { hashPassword } from '../utils/hashPassword.js';
+import pkg from 'pg';
+import 'dotenv/config';
+import { PrismaClient } from '@prisma/client';
+import jwt from 'jsonwebtoken';
+import { Router } from 'express';
+import bcrypt from 'bcrypt';
+const { Client } = pkg;
+const prisma = new PrismaClient();
+const router = Router();
+// Function to generate a refresh token
+const generateRefreshToken = (user) => {
+  return jwt.sign(
+    { userId: user.id },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: '7d' } // Refresh token valid for 7 days
+  );
+};
+//patient sets up profile
+export const profilepatient = async (req, res) => {
+  console.log('you are inside patient profile creation');
+  console.log('REFRESH_TOKEN_SECRET:', process.env.REFRESH_TOKEN_SECRET);
+  const { armyNo, password, confirmpassword } = req.body;
+  if (!armyNo || !password || !confirmpassword) {
+    res.send('ArmyNo,password required');
+  }
+  if (password != confirmpassword) {
+    res.send("Password doesn't match");
+  }
+  const user = await prisma.user.findFirst({ where: { armyNo } });
+  console.log(user);
+  if (!user) {
+    return res.status(400).send('Invalid army number or profile already set up');
+  }
+  try {
+    console.log('User refresh token:', user.refreshToken);
+    // Debugging statement
+    jwt.verify(user.refreshToken, process.env.REFRESH_TOKEN_SECRET);
+
+    console.log('Refresh token verified successfully'); // Debugging statement
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log(hashedPassword);
+    console.log(armyNo);
+
+    console.log('Password hashed successfully');
+    console.log('Updating user...'); // Debugging statement
+    const use = await prisma.user.findUnique({
+      where: { armyNo },
+    });
+    console.log(use);
+
+    const used = await prisma.user.update({
+      where: { armyNo: armyNo },
+
+      data: {
+        refreshToken: null,
+        password: hashedPassword,
+      },
+    });
+    console.log('User updated successfully');
+    res.send('Profile set up successfully');
+  } catch (error) {
+    if (error.name === 'TokenExpiredError') {
+      res.status(401).send('Refresh token expired');
+    } else if (error.name === 'JsonWebTokenError') {
+      res.status(500).send('Invalid refresh token');
+    } else {
+      res.status(500).send('Internal server error ');
+    }
+  }
+};
+//login patient
+export const loginpatient = async (req, res) => {
+  console.log('you are inside login system of patient');
+  const { armyNo, password } = req.body;
+  const user = await prisma.user.findUnique({
+    where: {
+      armyNo: armyNo,
+    },
+  });
+  bcrypt.compare(password, user.password, function (err, result) {
+    if (err) {
+      res.send('Password is incorrect');
+    } else if (result) {
+      res.json(user);
+    } else {
+      res.send('password is incorrect');
+    }
+  });
+};
+// Error handling middleware
+export const errorHandler = (err, req, res, next) => {
+  console.error(err.stack);
+  res.status(err.status || 500).json({
+    message: err.message || 'Internal Server Error',
+  });
+};
--- server/src/utils/apiError.js
+++ server/src/utils/apiError.js
@@ -1,4 +1,4 @@
-const HttpStatusCode = {
+export const HttpStatusCode = {
   OK: 200,
   BAD_REQUEST: 400,
   NOT_FOUND: 404,
@@ -6,62 +6,21 @@ const HttpStatusCode = {
 };
 
 class BaseError extends Error {
-  constructor(name, httpCode, description, isOperational) {
+  constructor(name, httpCode, description) {
     super(description);
     Object.setPrototypeOf(this, new.target.prototype);
 
     this.name = name;
     this.httpCode = httpCode;
-    this.isOperational = isOperational;
-
     // Capture stack trace for debugging purposes
     Error.captureStackTrace(this);
   }
 }
 
 class APIError extends BaseError {
-  constructor(name, httpCode = HttpStatusCode.INTERNAL_SERVER, isOperational = true, description = 'internal server error') {
-    super(name, httpCode, description, isOperational);
+  constructor(name, httpCode = HttpStatusCode.INTERNAL_SERVER, description = 'internal server error') {
+    super(name, httpCode, description);
   }
 }
 
 export { BaseError, APIError };
-
-// export enum HttpStatusCode {
-//   OK = 200,
-//   BAD_REQUEST = 400,
-//   NOT_FOUND = 404,
-//   INTERNAL_SERVER = 500,
-// }
-//
-// // Custom error class extending the built-in Error class
-// class BaseError extends Error {
-//   public readonly name: string;
-//   public readonly httpCode: HttpStatusCode;
-//   public readonly isOperational: boolean;
-//
-//   constructor(name: string, httpCode: HttpStatusCode, description: string, isOperational: boolean) {
-//     super(description);
-//     Object.setPrototypeOf(this, new.target.prototype);
-//
-//     this.name = name;
-//     this.httpCode = httpCode;
-//     this.isOperational = isOperational;
-//
-//     // Capture stack trace for debugging purposes
-//     Error.captureStackTrace(this);
-//   }
-// }
-//
-// class APIError extends BaseError {
-//   constructor(
-//     name: string,
-//     httpCode: HttpStatusCode = HttpStatusCode.INTERNAL_SERVER,
-//     isOperational: boolean = true,
-//     description: string = 'internal server error'
-//   ) {
-//     super(name, httpCode, description, isOperational);
-//   }
-// }
-//
-// export {BaseError, APIError}

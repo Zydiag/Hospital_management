d36e2997b7e59d7d69a619f56b6dc1e970810785
PratikSanap <99598917+PratikSanap@users.noreply.github.com>
2024-05-22 00:21:18 +0530

    Update patient.controller.js

--- server/src/controllers/patient.controller.js
+++ server/src/controllers/patient.controller.js
@@ -19,77 +19,78 @@ const generateRefreshToken = (user) => {
   );
 };
 //patient sets up profile
-export const profilepatient = async (req, res) => {
-  console.log('you are inside patient profile creation');
-  console.log('REFRESH_TOKEN_SECRET:', process.env.REFRESH_TOKEN_SECRET);
-  const { armyNo, password, confirmpassword } = req.body;
-  if (!armyNo || !password || !confirmpassword) {
-    res.send('ArmyNo,password required');
-  }
-  if (password != confirmpassword) {
-    res.send("Password doesn't match");
-  }
-  const user = await prisma.user.findFirst({ where: { armyNo } });
-  console.log(user);
-  if (!user) {
-    return res.status(400).send('Invalid army number or profile already set up');
-  }
-  try {
-    console.log('User refresh token:', user.refreshToken);
-    // Debugging statement
-    jwt.verify(user.refreshToken, process.env.REFRESH_TOKEN_SECRET);
-
-    console.log('Refresh token verified successfully'); // Debugging statement
-    const hashedPassword = await bcrypt.hash(password, 10);
-    console.log(hashedPassword);
-    console.log(armyNo);
-
-    console.log('Password hashed successfully');
-    console.log('Updating user...'); // Debugging statement
-    const use = await prisma.user.findUnique({
-      where: { armyNo },
-    });
-    console.log(use);
-
-    const used = await prisma.user.update({
-      where: { armyNo: armyNo },
-
-      data: {
-        refreshToken: null,
-        password: hashedPassword,
-      },
-    });
-    console.log('User updated successfully');
-    res.send('Profile set up successfully');
-  } catch (error) {
-    if (error.name === 'TokenExpiredError') {
-      res.status(401).send('Refresh token expired');
-    } else if (error.name === 'JsonWebTokenError') {
-      res.status(500).send('Invalid refresh token');
-    } else {
-      res.status(500).send('Internal server error ');
+export const profilepatient = async(req,res)=>{
+    console.log("you are inside patient profile creation");
+    console.log('REFRESH_TOKEN_SECRET:', process.env.REFRESH_TOKEN_SECRET);
+    const{armyNo,password,confirmpassword}=req.body;
+    if(!armyNo||!password||!confirmpassword){
+      res.send("ArmyNo,password required");
     }
-  }
-};
+    if(password!=confirmpassword){
+      res.send("Password doesn't match");
+    }
+    const user =await prisma.user.findFirst({where:{armyNo}});
+    console.log(user);
+    if(!user){
+        return res.status(400).send('Invalid army number or profile already set up');
+    }
+    try {
+      console.log('User refresh token:', user.refreshToken); 
+      // Debugging statement
+      jwt.verify(user.refreshToken, process.env.REFRESH_TOKEN_SECRET);
+      
+      console.log('Refresh token verified successfully'); // Debugging statement
+      const hashedPassword = await bcrypt.hash(password, 10);
+      console.log(hashedPassword);
+      console.log(armyNo);
+       
+        console.log('Password hashed successfully'); 
+        console.log('Updating user...');// Debugging statement
+        const use =await prisma.user.findUnique({
+          where:{armyNo}
+        })
+        console.log(use);
+        
+        const used=await prisma.user.update({
+       
+          where: { armyNo:armyNo },
+         
+          data: {
+            refreshToken:null,
+           password: hashedPassword,
+          }
+       });
+        console.log('User updated successfully'); 
+        res.send('Profile set up successfully');
+      } catch (error) {
+        if (error.name === 'TokenExpiredError') {
+          res.status(401).send('Refresh token expired');
+        } else if (error.name === 'JsonWebTokenError') {
+          res.status(500).send('Invalid refresh token');
+        } else {
+          res.status(500).send('Internal server error ');
+      }}
+
+}
 //login patient
-export const loginpatient = async (req, res) => {
-  console.log('you are inside login system of patient');
-  const { armyNo, password } = req.body;
-  const user = await prisma.user.findUnique({
-    where: {
-      armyNo: armyNo,
-    },
-  });
-  bcrypt.compare(password, user.password, function (err, result) {
-    if (err) {
-      res.send('Password is incorrect');
-    } else if (result) {
-      res.json(user);
-    } else {
-      res.send('password is incorrect');
-    }
-  });
-};
+export const loginpatient = async(req,res)=>{
+    console.log("you are inside login system of patient");
+    const {armyNo, password}=req.body;
+    const user=await prisma.user.findUnique({
+        where:{
+           armyNo:armyNo     
+        }
+    })
+    bcrypt.compare(password, user.password, function(err, result) {
+      if (err) {
+        res.send("Password is incorrect");
+      } else if(result) {
+        res.json(user);
+      } 
+      else{
+        res.send("password is incorrect");}
+    });
+}
 //perosnal-info-section1-patient
 export const getpersonalinfo=async(req,res)=>{
   console.log("we are inside getpersonalinfo route");
@@ -101,7 +102,7 @@ export const getpersonalinfo=async(req,res)=>{
   const user=await prisma.user.findFirst({
     where:{
       armyNo:armyNo,
-      date:date,
+      createdAt:new Date(date),
     },
     select:{
       armyNo:true,
@@ -125,21 +126,21 @@ export const getpersonalinfo=async(req,res)=>{
 export const getHealthRecord=async(req,res)=>{
   console.log("You are inside getHealthRecord Route");
   const {armyNo, date}=req.body;
-  const user =await prisma.user.findUnique({
+  const user =await prisma.user.findFirst({
     where:{
       armyNo:armyNo,
     }
   })
   const healthId=user.id;
-  const patient=await prisma.patient.findUnique({
+  const patient=await prisma.patient.findFirst({
     where:{
       userId:healthId
     }
   })
-  const health=await prisma.medical.findFirst({
+  const health=await prisma.Medical.findFirst({
     where:{
       patientId:patient.id,
-      date:date,
+      updatedAt:new Date(date),
     },
     select:{
      heightCm:true,
@@ -155,10 +156,77 @@ export const getHealthRecord=async(req,res)=>{
   }
   res.json(health);
 }
+//Read personal medical history
+export const getPersonalMedicalHistory=async(req,res)=>{
+  console.log("you are inside Personal Medical History Route");
+  const {armyNo,date}=req.body;
+  const user =await prisma.user.findFirst({
+    where:{
+      armyNo:armyNo,
+    }
+  })
+  const userId=user.id;
+  const patient=await prisma.patient.findFirst({
+    where:{
+      userId:userId,
+    }
+  })
+  const patientId=patient.id;
+  const treat=await prisma.treatment.findFirst({
+    where:{
+      patientId:patientId,
+      createdAt:new Date(date),
+    },
+    select:{
+       diagnosis:true,
+       note:true,
+       medicationName:true
+    }
+  })
+  if(!treat){
+    res.send("date is not present in database");
+  }
+  res.json(treat);
+}
+
+//Read Family History
+export const getFamilyHistory=async(req,res)=>{
+  console.log("you are inside familyhistory route");
+  const {armyNo,date}=req.body;
+  const user =await prisma.user.findFirst({
+   where:{
+    armyNo:armyNo
+   }
+  })
+  const userId=user.id;
+  console.log(userId);
+  const patient =await prisma.patient.findFirst({
+    where:{
+      userId:userId
+    }
+  })
+  const family=await prisma.familyHistory.findFirst({
+    where:{
+      patientId:patient.id,
+      createdAt:date
+    },
+    select:{
+      hypertension:true,
+      diabetesMellitus :true,
+      anyUnnaturalDeath:true,
+      otherSignificantHistory:true
+    }
+  })
+  if(!family){
+    res.send("date does not exist");
+  }
+  res.json(family);
+
+}
 // Error handling middleware
 export const errorHandler = (err, req, res, next) => {
-  console.error(err.stack);
-  res.status(err.status || 500).json({
-    message: err.message || 'Internal Server Error',
-  });
-};
+    console.error(err.stack);
+    res.status(err.status || 500).json({
+      message: err.message || 'Internal Server Error',
+    });
+  };

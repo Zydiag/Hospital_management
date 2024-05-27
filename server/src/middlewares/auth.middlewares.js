a4494725d98e628c89d516a2ac46ae85bdbfd2ee
PratikSanap <99598917+PratikSanap@users.noreply.github.com>
2024-05-27 11:08:55 +0530

    Update auth.middlewares.js

--- server/src/middlewares/auth.middlewares.js
+++ server/src/middlewares/auth.middlewares.js
@@ -1,13 +1,13 @@
-import { APIError } from '../utils/apiError.js';
+import { apiError } from '../utils/apiError.js';
 import { asyncHandler } from '../utils/asyncHandler.js';
 import db from '../lib/db.js';
 import Jwt from 'jsonwebtoken';
 
-export const verifyJwt = asyncHandler(async (req, _, next) => {
+export const verifyJwt = asyncHandler(async (req,res,next) => {
   try {
     const token = req.cookies?.refreshToken || req.header('Authorization')?.replace('Bearer ', '');
     if (!token) {
-      throw new APIError(400, 'Unauthorized request');
+      throw new apiError(400, 'Unauthorized request');
     }
     const decodedToken = Jwt.verify(token, process.env.REFRESH_TOKEN_SECRET);
    
@@ -17,19 +17,19 @@ export const verifyJwt = asyncHandler(async (req, _, next) => {
       },
     });
     if (!user) {
-      throw new APIError(401, 'invalid Access Token');
+      throw new apiError(401, 'invalid Access Token');
     }
     req.user = user;
     next();
   } catch (error) {
-    throw new APIError(401, error?.message || 'Invalid access token');
+    throw new apiError(401, error?.message || 'Invalid access token');
   }
 });
 
 export const authorizeDoctor = (req, res, next) => {
   if (req.user.role !== 'DOCTOR') {
     // return next(new APIError(HttpStatusCode.FORBIDDEN, 'Access denied'));
-    return next(new APIError(401, 'Access denied'));
+    return next(new apiError(401, 'Access denied'));
   }
   next();
 };
@@ -37,7 +37,7 @@ export const authorizeDoctor = (req, res, next) => {
 export const authorizeAdmin = (req, res, next) => {
   if (req.user.role !== 'ADMIN') {
     // return next(new APIError(HttpStatusCode.FORBIDDEN, 'Access denied'));
-    return next(new APIError(401, 'Access denied'));
+    return next(new apiError(401, 'Access denied'));
   }
   next();
 };

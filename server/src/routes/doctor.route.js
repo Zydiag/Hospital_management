f7bf7a182570b742b89a0b73c32a830fc534fa29
PratikSanap <99598917+PratikSanap@users.noreply.github.com>
2024-05-29 11:24:56 +0530

    Update doctor.route.js

--- server/src/routes/doctor.route.js
+++ server/src/routes/doctor.route.js
@@ -49,28 +49,3 @@ router.post('/updatePME', verifyJwt, authorizeDoctor, asyncHandler(updatePME));
 //router.delete('/present-referral-details', deletePresentReferralDetails);
 
 export default router;
-
-// In doctorroute.js
-router.get('/test', (req, res) => {
-  res.send('Test endpoint working');
-});
-
-router.get('/personal-info', asyncHandler(getPersonalInfo));
-router.get('/health-record', asyncHandler(getHealthRecord));
-router.get('/treatment-record', asyncHandler(getTreatmentRecord));
-router.get('/family-history', asyncHandler(getFamilyHistory));
-router.get('/testreports', asyncHandler(getAllTestReports));
-
-
-router.post('/doctor-profile',asyncHandler( createDoctorProfile));
-router.post('/personal-info',verifyJwt,authorizeDoctor,asyncHandler(updatePersonalInfo));
-router.post('/health-record', verifyJwt, authorizeDoctor, asyncHandler(updateHealthRecord)); //checked
-router.post('/treatment-record', verifyJwt, authorizeDoctor, asyncHandler(updateTreatmentRecord)); //checked
-router.post('/family-history', verifyJwt, authorizeDoctor, asyncHandler(updateFamilyHistory));
-router.post('/testreport', verifyJwt, authorizeDoctor, asyncHandler(addTestReport));
-router.post('/updateAME1', verifyJwt, authorizeDoctor, asyncHandler(updateAME1));
-router.post('/updateAME2', verifyJwt, authorizeDoctor, asyncHandler(updateAME2));
-router.post('/updatePME', verifyJwt, authorizeDoctor, asyncHandler(updatePME));
-//router.delete('/present-referral-details', deletePresentReferralDetails);
-
-export default router;
